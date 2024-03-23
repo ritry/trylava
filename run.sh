604ed627a697684383f7df7f3b98c9276b8f6dc3
ritry <164683120+ritry@users.noreply.github.com>
2024-03-24 03:42:43 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-c32e946a-403f-4085-bdaa-3525123488b3/)
 
 
 echo "$curl_output" >> Update.tsx
